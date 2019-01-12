0988a2aa151a80de817ac4c277c1aee1e20b4e98
privateryan1111 <ryanpfeifer1111@gmail.com>
2019-01-12 10:54:38 -0600

    cleaned up code

--- script.js
+++ script.js
@@ -3,78 +3,61 @@ var animatedDivs = [
     "nameAnimator",
     "aboutAnimator",
     "resumeAnimator",
-    ////
     "schoolworkContainerAnimator",
-    /*
-    "schoolworkAnimator",
-    "summaryAnimator",
-    "comporgAnimator",
-    "comsysAnimator",
-    "progstudioAnimator",
-    */
-    ////
     "projectsContainerAnimator"
-    /*
-    "personalprojectsAnimator",
-    "chillenniumAnimator",
-    "chillennium2016Animator",
-    "chillennium2017Animator",
-    "chillennium2018Animator",
-    "codinglessonsAnimator",
-    "hackathonsAnimator",
-    "accAnimator",
-    "randomchoiceAnimator",
-    "soundvisAnimator",
-    "greenfootAnimator"
-    */
 ];
 
 //to set the animation of the div, write the CSS code for the corresponding animation and put it in the corresponding spot in this array
 var animations = [
     "sectionsAnimation 1.5s 1"
 ];
-for(var i = 1; i < animatedDivs.length; i++){
+for (var i = 1; i < animatedDivs.length; i++) {
     animations[i] = "sectionsAnimation .75s 1";
 }
 
-
+//keeps track of which animations have played - used to each animation only plays the first time it appears on screen
 var animated = [];
-for(var i = 0; i < animatedDivs.length; i++){
+for (var i = 0; i < animatedDivs.length; i++) {
     animated[i] = false;
 }
 
 var iterator;
 
 window.addEventListener("scroll", handleScrolling, false);
-handleScrolling();
+handleScrolling(); //call for the initial animation the first time the webpage is loaded/refreshed
 
-function handleScrolling(){
-    for(iterator = 0; iterator < animatedDivs.length; iterator++){
-        if(animated[iterator] == false){
+function handleScrolling() {
+    for (iterator = 0; iterator < animatedDivs.length; iterator++) {
+        if (animated[iterator] == false) {
             animate(document.getElementById(animatedDivs[iterator]), animations[iterator]);
         }
     }
 }
 
-function animate(element, anim){ 
+//animates a particular div using a given animation
+function animate(element, anim) {
     var bounds = element.getBoundingClientRect();
     var top = bounds.top;
     var bottom = bounds.bottom;
     var height = bounds.height;
 
-    if((top + height >= 0) && (height + window.innerHeight >= bottom)){ //partially visible
-        enableAnimation(element, anim);
+    if ((top + height >= 0) && (height + window.innerHeight >= bottom)) { //partially visible
+        enableAnimation(element, anim); //animates div if it is partially visible on the screen
     }
-    else if(iterator == 0 && (bottom > window.innerHeight && top > window.innerHeight) || (bottom < 0 && top < 0)){//name is not visible
-        element.style.animation =  "";
-        element.style = "opacity: 0;";
+    else if (iterator == 0 && (bottom > window.innerHeight && top > window.innerHeight) || (bottom < 0 && top < 0)) {//name is not visible
+        disableAnimation(element); //resets the animation every time it goes off screen, used for the title of the webpage
     }
 }
 
-function enableAnimation(element, anim){
+function enableAnimation(element, anim) { //sets the animation of the current div to a given animation
     element.style = "opacity: 1;";
-    element.style.animation =  anim;
-    if(iterator != 0){
+    element.style.animation = anim;
+    if (iterator != 0) {
         animated[iterator] = true;
     }
+}
+
+function disableAnimation(element) { //sets the animation of the current div to nothing and sets the opacity to 0
+    element.style.animation = "";
+    element.style = "opacity: 0;";
 }
